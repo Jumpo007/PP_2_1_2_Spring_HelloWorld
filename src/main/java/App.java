0a9a9196bef9108e9c419b4f12cafbb0feb64640
Jumpo007 <78562147+Jumpo007@users.noreply.github.com>
2022-08-09 20:53:02 +0300

    я сделаль

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getMessage());
+        System.out.println( bean3 == bean4);
+
     }
 }
