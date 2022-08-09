148fd1d16668325ec85ae251ab233b78e7240f23
Jumpo007 <78562147+Jumpo007@users.noreply.github.com>
2022-08-09 20:54:52 +0300

    я сделаль 2

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -6,7 +6,5 @@ public class Cat {
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    public void setMessage(String message) { this.message = message; }
 }
